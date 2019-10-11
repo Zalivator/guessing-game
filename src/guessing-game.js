b3d512e7326d7a7e8821ade4ec0e7a7d4d0804fe
Zalya <pzalivako@mail.ru>
2019-10-11 22:40:47 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minimal = min;
+        this.maximal = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.maximal + this.minimal) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.maximal = this.middle;
     }
 
     greater() {
-
+        this.minimal = this.middle;
     }
 }
 
